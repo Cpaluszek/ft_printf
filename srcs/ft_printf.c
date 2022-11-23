b58e88cfcddc5d20a1cda408a4750c84e4a2b3e2
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2022-11-23 17:32:44 +0100

    Add files via upload

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 15:07:19 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/11/16 10:38:56 by cpalusze         ###   ########.fr       */
+/*   Updated: 2022/11/22 13:49:33 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,16 @@ int	ft_printf(const char *input, ...)
 	va_start(args, input);
 	i = 0;
 	printed_char_count = ft_strlen(input);
+	if (write(STDOUT_FILENO, 0, 0) == -1)
+		return (-1);
 	while (input[i])
 	{
 		if (input[i] == '%')
 			printed_char_count += ft_pick_conversion(input[++i], args) - 2;
 		else
 			ft_putchar_fd(input[i], STDOUT_FILENO);
-		i++;
+		if (input[i])
+			i++;
 	}
 	va_end(args);
 	return (printed_char_count);
